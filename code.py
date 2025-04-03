1b0881363b9e44a894d6c6098795850b3e9e200e
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-04-04 00:36:32 +0300

    add discount calculation function

---
+++ code.py
@@ -0,0 +1,13 @@
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        final_price = price - discount_amount
+    else:
+        final_price = price
+    return final_price
+
+original_price = float(input("Enter the original price: "))
+discount_percent = int(input("Enter the discount percentage: "))
+
+final_price = calculate_discount(original_price, discount_percent)
+print("The final price after applying the discount is:", final_price)
